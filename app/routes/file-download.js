0392ee2b6cac1d89787afc95997555a7f4a69bbc
JungaBunga <amir.ishmuhametov@gmail.com>
2024-08-06 13:57:43 +0100

    refactored the upload and download to follow design principles

--- app/routes/file-download.js
+++ app/routes/file-download.js
@@ -1,39 +1,36 @@
 const { BlobServiceClient } = require('@azure/storage-blob')
 const config = require('../config/storage')
 
-const downloadFile = async (request, h) => {
-  const { uniqueId } = request.params
-
-  try {
-    const blobServiceClient = BlobServiceClient.fromConnectionString(config.connectionStr)
-    const containerClient = blobServiceClient.getContainerClient(config.container)
-
-    const blobName = `${config.folder}/${uniqueId}`
-
-    const blockBlobClient = containerClient.getBlockBlobClient(blobName)
-
-    const exists = await blockBlobClient.exists()
-    if (!exists) {
-      return h.response({ error: 'File not found' }).code(404)
-    }
-
-    const properties = await blockBlobClient.getProperties()
-    const filename = properties.metadata.filename || 'downloaded-file'
-
-    const downloadBlockBlobResponse = await blockBlobClient.download(0)
-    const stream = downloadBlockBlobResponse.readableStreamBody
-
-    return h.response(stream)
-      .header('Content-Disposition', `attachment; filename="${filename}"`)
-      .header('Content-Type', properties.contentType || 'application/octet-stream')
-  } catch (error) {
-    console.error('Error downloading file:', error.message)
-    return h.response({ error: 'File download failed', details: error.message }).code(500)
-  }
-}
-
 module.exports = {
   method: 'GET',
   path: '/download/{uniqueId}',
-  handler: downloadFile
+  handler: async (request, h) => {
+    const { uniqueId } = request.params
+    try {
+      const blobServiceClient = BlobServiceClient.fromConnectionString(config.connectionStr)
+      const containerClient = blobServiceClient.getContainerClient(config.container)
+
+      const blobName = `${config.folder}/${uniqueId}`
+
+      const blockBlobClient = containerClient.getBlockBlobClient(blobName)
+
+      const exists = await blockBlobClient.exists()
+      if (!exists) {
+        return h.response({ error: 'File not found' }).code(404)
+      }
+
+      const properties = await blockBlobClient.getProperties()
+      const filename = properties.metadata.filename || 'downloaded-file'
+
+      const downloadBlockBlobResponse = await blockBlobClient.download(0)
+      const stream = downloadBlockBlobResponse.readableStreamBody
+
+      return h.response(stream)
+        .header('Content-Disposition', `attachment; filename="${filename}"`)
+        .header('Content-Type', properties.contentType || 'application/octet-stream')
+    } catch (error) {
+      console.error('Error downloading file:', error.message)
+      return h.response({ error: 'File download failed', details: error.message }).code(500)
+    }
+  }
 }
--- app/routes/file-upload.js
+++ app/routes/file-upload.js
@@ -6,67 +6,6 @@ const { handleFileLimitExceeded } = require('../utils/file-checks/lenght-of-file
 // const avConfig = require('../config/av-scan')
 // const { getAVToken } = require('../utils/av-scan/get-av-token')
 // const { sendToAvScan } = require('../utils/av-scan/send-to-AV')
-
-const uploadFile = async (request, h) => {
-  const { payload } = request
-  const { files, collection } = payload
-
-  if (!Array.isArray(files)) {
-    return h.response({ error: 'Files should be an array' }).code(400)
-  }
-  try {
-    handleFileLimitExceeded(files)
-  } catch (error) {
-    return h.response({ error: error.message }).code(400)
-  }
-
-  try {
-    const blobServiceClient = BlobServiceClient.fromConnectionString(storageConfig.connectionStr)
-    const containerClient = blobServiceClient.getContainerClient(storageConfig.container)
-    // const token = await getAVToken()
-    const results = []
-
-    for (const file of files) {
-      try {
-        checkFileExtension(file.hapi.filename)
-      } catch (error) {
-        results.push({ error: error.message, fileName: file.hapi.filename })
-        continue
-      }
-      // const scanResult = await sendToAvScan(token, file)
-      const scanResult = { isSafe: true }
-
-      if (!scanResult.isSafe) {
-        results.push({ error: 'File is malicious and has been rejected', fileName: file.hapi.filename })
-        continue
-      }
-
-      const uniqueId = uuidv4()
-      const blobName = `${storageConfig.folder}/${uniqueId}`
-      const blockBlobClient = containerClient.getBlockBlobClient(blobName)
-
-      await blockBlobClient.uploadStream(file)
-
-      const metadata = {
-        filename: file.hapi.filename,
-        blobReference: uniqueId,
-        scheme: payload.scheme,
-        collection
-      }
-
-      // Update blob metadata
-      await blockBlobClient.setMetadata(metadata)
-
-      results.push({ message: 'File uploaded successfully', metadata })
-    }
-
-    return h.response(results).code(201)
-  } catch (error) {
-    console.error('Error uploading files:', error.message)
-    return h.response({ error: 'File upload failed', details: error.message }).code(500)
-  }
-}
-
 module.exports = {
   method: 'POST',
   path: '/upload',
@@ -79,5 +18,52 @@ module.exports = {
       maxBytes: 1530485760
     }
   },
-  handler: uploadFile
+  handler: async (request, h) => {
+    const { payload } = request
+    const { files, collection } = payload
+    console.log('Payload:', files)
+    const filesArray = Array.isArray(files) ? files : [files]
+    try {
+      handleFileLimitExceeded(filesArray)
+    } catch (error) {
+      return h.response({ error: error.message }).code(400)
+    }
+    try {
+      const blobServiceClient = BlobServiceClient.fromConnectionString(storageConfig.connectionStr)
+      const containerClient = blobServiceClient.getContainerClient(storageConfig.container)
+      // const token = await getAVToken()
+      const results = []
+      for (const file of filesArray) {
+        try {
+          checkFileExtension(file.hapi.filename)
+        } catch (error) {
+          results.push({ error: error.message, fileName: file.hapi.filename })
+          continue
+        }
+        // const scanResult = await sendToAvScan(token, file)
+        const scanResult = { isSafe: true }
+        if (!scanResult.isSafe) {
+          results.push({ error: 'File is malicious and has been rejected', fileName: file.hapi.filename })
+          continue
+        }
+        const uniqueId = uuidv4()
+        const blobName = `${storageConfig.folder}/${uniqueId}`
+        const blockBlobClient = containerClient.getBlockBlobClient(blobName)
+        await blockBlobClient.uploadStream(file)
+        const metadata = {
+          filename: file.hapi.filename,
+          blobReference: uniqueId,
+          scheme: payload.scheme,
+          collection
+        }
+        // Update blob metadata
+        await blockBlobClient.setMetadata(metadata)
+        results.push({ message: 'File uploaded successfully', metadata })
+      }
+      return h.response(results).code(201)
+    } catch (error) {
+      console.error('Error uploading files:', error.message)
+      return h.response({ error: 'File upload failed', details: error.message }).code(500)
+    }
+  }
 }
