cb4547d38928f1c0ca142b042af1e072db757723
Rana Salem <ranatasalem@gmail.com>
2024-08-08 12:04:18 +0100

    wip: service bus config

--- app/config/index.js
+++ app/config/index.js
@@ -1,9 +1,11 @@
 const serverConfig = require('./server')
 const storageConfig = require('./storage')
-const avConfig = require('./av-scan')
+// const avConfig = require('./av-scan')
+const messageConfig = require('./message')
 
 module.exports = {
   serverConfig,
   storageConfig,
-  avConfig
+  // avConfig,
+  messageConfig
 }
--- app/index.js
+++ app/index.js
@@ -2,11 +2,13 @@ require('./insights').setup()
 require('log-timestamp')
 const { createServer } = require('./server')
 const { createAzuriteInfrastructure } = require('./blob-storage')
+const { startMessaging } = require('./message')
 
 const init = async () => {
   const server = await createServer()
   await server.start()
   console.log('Server running on %s', server.info.uri)
+  await startMessaging()
   await createAzuriteInfrastructure()
 }
 
