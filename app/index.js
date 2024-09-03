a2f3fd2b0a1cd71d5fbd64bc2967e141f6d09a5b
JungaBunga <amir.ishmuhametov@gmail.com>
2024-09-03 12:28:34 +0100

    pushing

--- app/index.js
+++ app/index.js
@@ -2,12 +2,16 @@ require('./insights').setup()
 require('log-timestamp')
 const { createServer } = require('./server')
 const { connectToBlob } = require('./storage')
+const { startMessaging } = require('./message')
 
 const init = async () => {
   const server = await createServer()
   await server.start()
   console.log('Server running on %s', server.info.uri)
   await connectToBlob()
+  console.log('Connected to blob storage')
+  await startMessaging()
+  console.log('Started messaging')
 }
 
 process.on('unhandledRejection', (err) => {
