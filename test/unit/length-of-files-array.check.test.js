45c458d21c79620fda3665c28bf19ade8b7877c6
Rana Salem <ranatasalem@gmail.com>
2024-09-06 13:42:24 +0100

    add passing tests with 1 alternative example

--- test/unit/length-of-files-array.check.test.js
+++ test/unit/length-of-files-array.check.test.js
@@ -2,18 +2,17 @@ const { MAX_FILES } = require('../../app/constants/max-files')
 const { handleFileLimitExceeded } = require('../../app/utils/file-checks/lenght-of-files-array-check')
 
 describe('check length of files array', () => {
-  test('should throw an error when files array length is greater than MAX_FILES', () => {
+  test('should throw an error when files array length is greater than MAX_FILES', async () => {
     const result = handleFileLimitExceeded([1, 2, 3, 4])
-    expect(result.error).toThrow(`Uploaded files must be less than ${MAX_FILES} files.`)
+    await expect(result).rejects.toThrow(`Uploaded files must be less than ${MAX_FILES} files.`)
   })
 
-  test('should not throw an error when files array length is equal to MAX_FILES', () => {
+  test('should not throw an error when files array length is equal to MAX_FILES', async () => {
     const result = handleFileLimitExceeded([0, 1, 2])
-    expect(result.error).toBeUndefined()
+    await expect(result).resolves.toBeUndefined()
   })
 
-  test('should not throw an error when files array length is less than MAX_FILES', () => {
-    const result = handleFileLimitExceeded([0, 1])
-    expect(result.error).toBeUndefined()
+  test('should not throw an error when files array length is less than MAX_FILES', async () => {
+    await expect(handleFileLimitExceeded([0, 1])).resolves.toBeUndefined()
   })
 })
