40ee596c1e8dcc1948e29b947cca7070a79727e0
Rana Salem <ranatasalem@gmail.com>
2024-09-06 13:09:29 +0100

    wip

--- test/unit/length-of-files-array.check.test.js
+++ test/unit/length-of-files-array.check.test.js
@@ -1,11 +1,11 @@
-// const { MAX_FILES } = require('../../app/constants/max-files')
+const { MAX_FILES } = require('../../app/constants/max-files')
 const { handleFileLimitExceeded } = require('../../app/utils/file-checks/lenght-of-files-array-check')
 
 describe('check length of files array', () => {
-  // test('should throw an error when files array length is greater than MAX_FILES', () => {
-  //   const result = handleFileLimitExceeded([1, 2, 3, 4, 5])
-  //   expect(result.error).toBe(`Uploaded files must be less than ${MAX_FILES} files.`)
-  // })
+  test('should throw an error when files array length is greater than MAX_FILES', () => {
+    const result = handleFileLimitExceeded([1, 2, 3, 4])
+    expect(result.error).toThrow(`Uploaded files must be less than ${MAX_FILES} files.`)
+  })
 
   test('should not throw an error when files array length is equal to MAX_FILES', () => {
     const result = handleFileLimitExceeded([0, 1, 2])
